eb3c7d897b691bdc0ef916af3248268af53b02df
Roosevelt de Souza <rooseveltsouzav12@gmail.com>
2025-04-02 14:57:01 -0300

    adiciona estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-// import './App.css'
-
 import './global.css'
-// import Styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
-    </p>
+    <div></div>
   )
 }
 
