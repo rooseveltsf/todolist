ca6caf4a8e20f8a549e11f8ad14b1a906b733b89
Roosevelt de Souza <rooseveltsouzav12@gmail.com>
2025-04-03 16:14:54 -0300

    cria componente TaskItem

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,7 +1,15 @@
+import { Trash } from "@phosphor-icons/react"
+
+import TaskItemStyles from './TaskItem.module.css'
+
 function TaskItem() {
   return (
-    <div>
-      <h1>TaskItem</h1>
+    <div className={TaskItemStyles.containerItem}>
+      <input type="checkbox" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <button>
+        <Trash size={24} weight="bold" />
+      </button>
     </div>
   )
 }
--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,11 +1,15 @@
 import EmptyList from '../EmptyList'
+import TaskItem from '../TaskItem'
 
 import TaskListStyles from './TaskList.module.css'
 
 function TaskList() {
     return (
         <div className={TaskListStyles.container}>
-            <EmptyList />
+            {/* <EmptyList /> */}
+            <TaskItem />
+            <TaskItem />
+            <TaskItem />
         </div>
     )
 }
